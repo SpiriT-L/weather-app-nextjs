5fe7750122d872fb809c9b982206288b1645f33e
LeonID <lmatskevich79@gmail.com>
2025-09-22 23:18:46 +0300

    feat: update ESLint configuration and add TypeScript support

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,11 @@ import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import { FlatCompat } from '@eslint/eslintrc';
 
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsparser from '@typescript-eslint/parser';
+import reactHooks from 'eslint-plugin-react-hooks';
+import prettierPlugin from 'eslint-plugin-prettier';
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
@@ -12,16 +17,52 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends(
     'next',
-    'prettier',
+    'plugin:jsx-a11y/recommended',
     'next/core-web-vitals',
-    'next/typescript'
+    'prettier'
   ),
   {
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      parser: tsparser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        project: './tsconfig.json',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+    },
     rules: {
-      'no-console': 'warn',
-      'no-var': 'error',
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        { argsIgnorePattern: '^_' },
+      ],
       '@typescript-eslint/no-explicit-any': 'error',
     },
+  },
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    plugins: {
+      'react-hooks': reactHooks,
+      prettier: prettierPlugin,
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+    rules: {
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+      'prettier/prettier': 'error',
+      'no-console': 'warn',
+      'no-var': 'error',
+      
+    },
+  },
+  {
     ignores: [
       'node_modules/**',
       '.next/**',
