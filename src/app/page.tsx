c2fe73092471c5baaa9af60d1ab3fae3ac8af2a8
LeonID <lmatskevich79@gmail.com>
2025-09-25 06:37:45 +0300

    feat: install axios and react-icons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
+import axios from 'axios';
+import { useState } from 'react';
+import { BsSearch } from 'react-icons/bs';
+
 export default function Home() {
+  const [city, setCity] = useState('');
+  const [weather, setWeather] = useState({});
+  const [loading, setLoading] = useState(false);
+
   const url = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}`;
 
   return (
     <>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <BsSearch />
     </>
   );
 }
